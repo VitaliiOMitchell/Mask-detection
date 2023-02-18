007386dfc34c06e8e342e8345bb854a59a13d616
VitaliiOMitchell <omitchellone@gmail.com>
2023-02-18 17:59:06 +0200

    I changed everything

--- data_extraction.py
+++ data_extraction.py
@@ -1,144 +1,52 @@
 from bs4 import BeautifulSoup
 import os
 import pickle
+import numpy as np
 
-def data_extraction(path):
-    box_data = []
-    
-    for annot in os.listdir(path):
-        annot = os.path.join(path, annot)
-        with open(annot, 'r') as file:
-            content = file.readlines()
-        content = ''.join(content)
-        
-        soup = BeautifulSoup(content,'lxml')
-        
-        xmin_data = soup.findAll('xmin')
-        ymin_data = soup.findAll('ymin')
-        xmax_data = soup.findAll('xmax')
-        ymax_data = soup.findAll('ymax')
-        xmin = []
-        ymin = []
-        xmax = []
-        ymax = []
-        for coordinate_x in xmin_data:
-            coordinate_x = str(coordinate_x)
-            for i in range(len(coordinate_x)-1):
-                try:
-                    if len(coordinate_x) == 14:
-                        elem = coordinate_x[i] 
-                        elem = int(elem)
-                        xmin.append(elem)
-                    elif len(coordinate_x) == 15:
-                        elem = coordinate_x[i] + coordinate_x[i+1]
-                        elem = int(elem)
-                        xmin.append(elem)
-                    elif len(coordinate_x) == 16:
-                        elem = coordinate_x[i] + coordinate_x[i+1] + coordinate_x[i+2]
-                        elem = int(elem)
-                        xmin.append(elem)
-                except:
-                    pass
-        
-        for coordinate_y in ymin_data:
-            coordinate_y = str(coordinate_y)
-            for i in range(len(coordinate_y)-1):
-                try:
-                    if len(coordinate_y) == 14:
-                        elem = coordinate_y[i] 
-                        elem = int(elem)
-                        ymin.append(elem)
-                    elif len(coordinate_y) == 15:
-                        elem = coordinate_y[i] + coordinate_y[i+1]
-                        elem = int(elem)
-                        ymin.append(elem)
-                    elif len(coordinate_y) == 16:
-                        elem = coordinate_y[i] + coordinate_y[i+1] + coordinate_y[i+2]
-                        elem = int(elem)
-                        ymin.append(elem)
-                except:
-                    pass
-        
-        for coordinate_x in xmax_data:
-            coordinate_x = str(coordinate_x)
-            for i in range(len(coordinate_x)-1):
-                try:
-                    if len(coordinate_x) == 14:
-                        elem = coordinate_x[i] 
-                        elem = int(elem)
-                        xmax.append(elem)
-                    elif len(coordinate_x) == 15:
-                        elem = coordinate_x[i] + coordinate_x[i+1]
-                        elem = int(elem)
-                        xmax.append(elem)
-                    elif len(coordinate_x) == 16:
-                        elem = coordinate_x[i] + coordinate_x[i+1] + coordinate_x[i+2]
-                        elem = int(elem)
-                        xmax.append(elem)
-                except:
-                    pass
-        
-        for coordinate_y in ymax_data:
-            coordinate_y = str(coordinate_y)
-            for i in range(len(coordinate_y)-1):
-                try:
-                    if len(coordinate_y) == 14:
-                        elem = coordinate_y[i] 
-                        elem = int(elem)
-                        ymax.append(elem)
-                    elif len(coordinate_y) == 15:
-                        elem = coordinate_y[i] + coordinate_y[i+1]
-                        elem = int(elem)
-                        ymax.append(elem)
-                    elif len(coordinate_y) == 16:
-                        elem = coordinate_y[i] + coordinate_y[i+1] + coordinate_y[i+2]
-                        elem = int(elem)
-                        ymax.append(elem)
-                except:
-                    pass
-        
-        data_dict = {'xmin': xmin,
-                    'ymin': ymin,
-                    'xmax': xmax,
-                    'ymax': ymax}
-        im_info = (annot[46:], data_dict)
-        box_data.append(im_info)
+class Data_extractor:
+    def data_extraction(self, data_path):
+        data = []
+        path = data_path
+        for annot in os.listdir(path):
+            file = os.path.join(path, annot)
+            with open(file, 'r') as f:
+                content = f.readlines()
+            content = ''.join(content)
+            soup = BeautifulSoup(content, 'lxml')
+            xmin_data = soup.find_all('xmin')
+            ymin_data = soup.find_all('ymin')
+            xmax_data = soup.find_all('xmax')
+            ymax_data = soup.find_all('ymax')
+            labels = soup.find_all('name')
+            
+            box_data = np.array([self.labels(labels),
+                                 [self.box(xmin_data), 
+                                 self.box(ymin_data),
+                                 self.box(xmax_data),
+                                 self.box(ymax_data)]], dtype=np.ndarray)
+            box_data[1] = np.array(box_data[1]).T
+            data.append(box_data)
+            
+        return data
 
-    return box_data
-
-def labels_extraction(path):
-    labels_arr = []
+    def box(self, boxes):
+        coordinates = []
+        for data in boxes:
+            val = int(data.text)
+            coordinates.append(val)
+        return coordinates
     
-    for annot in os.listdir(path):
-        annot = os.path.join(path, annot)
-        with open(annot, 'r') as data:
-            content = data.readlines()
-        content = ''.join(content)
-        soup = BeautifulSoup(content, 'lxml')
-        data = soup.find_all('name')
-        labels = {'with_mask': 0, 
-                'without_mask': 0, 
-                'mask_weared_incorrect': 0
-                }
-        for val in data:
-            val = str(val)
-            if 'with_mask' in val:
-                labels['with_mask'] += 1
-            elif 'without_mask' in val:
-                labels['without_mask'] += 1
-            elif 'mask_weared_incorrect' in val:
-                labels['mask_weared_incorrect'] += 1
-        labels_arr.append(labels) 
-    
-    return labels_arr
+    def labels(self, labels):
+        labels_arr = []
+        for label in labels:
+            labels_arr.append(label.text)
+        return tuple(labels_arr)
 
 
 if __name__ == '__main__':
-    annot_path = os.path.join('F:/Python/Projects/Mask-detection/annot_masks')
-    box_data = data_extraction(annot_path)
-    labels = labels_extraction(annot_path)
+    annot_path = 'F:/Python/Projects/Mask-detection/annot_masks'
+    extractor = Data_extractor()
+    output = extractor.data_extraction(annot_path)
+    with open('data_for_detection.pkl', 'wb') as d:
+        pickle.dump(output, d)
     
-    with open('box_data.pkl', 'wb') as bd:
-        pickle.dump(box_data, bd)
-    with open('labels.pkl', 'wb') as l:
-        pickle.dump(labels, l)
